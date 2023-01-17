c3db4655c1ab9e844ff5021cd831bedb4b7f635b
Abdelfattah Hilmi <hilmiabdelfattah@gmail.com>
2023-01-17 18:02:29 +0100

    jwt Auth setup

--- Users/UsersManagement/urls.py
+++ Users/UsersManagement/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path 
 from .views import UserRegister
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('register/', UserRegister.as_view(), name = 'User_registration_endpoint' ),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
