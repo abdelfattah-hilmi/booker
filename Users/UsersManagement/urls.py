6382aab5f062800b5368836e3fe657d249c01cd3
Abdelfattah Hilmi <hilmiabdelfattah@gmail.com>
2023-01-20 19:28:39 +0100

    sample of user profile view

--- Users/UsersManagement/urls.py
+++ Users/UsersManagement/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from .views import UserRegister
+from .views import UserRegisterView,UserProfileView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
-    path('register/', UserRegister.as_view(), name = 'User_registration_endpoint' ),
+    path('register/', UserRegisterView.as_view(), name = 'User_registration_endpoint' ),
+    path('User/<str:id>', UserProfileView.as_view(), name = 'User_registration_endpoint' ),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
--- Users/UsersManagement/views.py
+++ Users/UsersManagement/views.py
@@ -1,10 +1,22 @@
 from .models import User
 from .serializers import UserSerializer
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 
-class UserRegister(ListCreateAPIView):
+class UserRegisterView(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserProfileView(RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'id'
+    queryset = User.objects.filter(id='1')
+
+
+
 
 
 # TODO Create an endpoint for other services to check the validity and usability of the jwt
