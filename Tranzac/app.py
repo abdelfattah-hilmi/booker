bbb108ddd8cb60de0b43b14e638794a8244adf73
Abdelfattah Hilmi <hilmiabdelfattah@gmail.com>
2023-01-25 00:16:37 +0100

    minor changes

--- Tranzac/app.py
+++ Tranzac/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_mongoengine import MongoEngine
 from models.venue import Venue
+from bson import ObjectId
 
 
 app = Flask(__name__)
@@ -20,8 +21,14 @@ def create_venue(title,rating):
     Venue(title=title,rating=rating).save()
 
 
-@app.route('/', methods=['GET','Post'])
+@app.get('/')
 def main_endpoint():
+    return "Hello there! This is the transac service",200
+
+
+
+@app.route('/venues', methods=['GET','Post'])
+def list_create_venues():
     if request.method == 'POST':
         try:
             request_data = request.get_json()
@@ -30,13 +37,19 @@ def main_endpoint():
             create_venue(venue_title,venue_rating)
             return request_data,201
         except:
-            return 400
+            return "Value Error",400
 
 
     else:
-        venues = jsonify( Venue.objects() )
+        venues = jsonify( Venue.objects )
         return venues
 
+
+@app.get('/venues/<int:id>')
+def get_venue(id):
+    venues = Venue.objects
+    return (jsonify(venues[id]) if id < len(venues) else "The requested object does not exist",404)
+    
     
 if __name__ == '__main__':
     app.run(debug=True)
